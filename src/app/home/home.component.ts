994ad9c201997e186849c583067156c25697e926
s87771 <s87771@bht-berlin.de>
2024-04-09 21:56:46 +0200

    modifications, new weather pictures added depending on the current weather

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,12 +22,14 @@ export class HomeComponent {
   constructor(private store: Store<any>, private weatherApiService: WeatherApiService, private cities: CitiesService) { }
   citiesList: string[] = this.cities.bigCities;
 
-  city: string = 'berlin';
   weather: any;
+  cureentWeatherImage = "";
+  city: string = 'berlin';
 
   ngOnInit() {
     this.weatherApiService.getCurrentWeather(this.city).subscribe((weather: any) => {
       this.weather = weather;
+      this.updateWeatherImage();
     });
   }
 
@@ -36,7 +38,31 @@ export class HomeComponent {
     this.city = city;
     this.weatherApiService.getCurrentWeather(this.city).subscribe((weather: any) => {
       this.weather = weather;
+      this.updateWeatherImage();
     });
   }
 
+  updateWeatherImage() {
+    if (this.weather && this.weather.current && this.weather.current.condition.text) {
+      if (this.weather.current.condition.text === "Sunny") {
+        this.cureentWeatherImage = "../../assets/sunny.png";
+      } else if (this.weather.current.condition.text === "Partly cloudy" && this.weather.current.is_day == 0) {
+        this.cureentWeatherImage = "../../assets/night.png";
+      } else if (this.weather.current.condition.text === "Partly cloudy" && this.weather.current.is_day == 1) {
+        this.cureentWeatherImage = "../../assets/partlyCloud.png";
+      } else if (this.weather.current.condition.text === "Cloudy" || this.weather.current.condition.text === "Overcast") {
+        this.cureentWeatherImage = "../../assets/cloud.png";
+      } else if (this.weather.current.condition.text === "Mist") {
+        this.cureentWeatherImage = "../../assets/mist.png";
+      } else if (this.weather.current.condition.text === "Patchy rain possible" || this.weather.current.condition.text === "Light rain" || this.weather.current.condition.text === "Moderate rain" || this.weather.current.condition.text === "Heavy rain at times" || this.weather.current.condition.text === "Light freezing rain" || this.weather.current.condition.text === "Torrential rain shower") {
+        this.cureentWeatherImage = "../../assets/rain.png";
+      } else if (this.weather.current.condition.text === "Heavy snow" || this.weather.current.condition.text === "Ice pellets" || this.weather.current.condition.text === "Light sleet showers") {
+        this.cureentWeatherImage = "../../assets/mist.png";
+      } else if (this.weather.current.condition.text === "Clear") {
+        this.cureentWeatherImage = "../../assets/moon.png";
+      } else {
+        this.cureentWeatherImage = this.weather.current.condition.icon;
+      }
+    }
+  }
 }
