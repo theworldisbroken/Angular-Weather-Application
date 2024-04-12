dbd9afdb232cefc3630fc4647f932095fe47780f
s87771 <s87771@bht-berlin.de>
2024-04-12 14:58:50 +0200

    icon added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [{
     path: '',
     component: HomeComponent,
-    title: 'Home page'
+    title: 'Theworld Weather'
 },
 {
     path: '**',
